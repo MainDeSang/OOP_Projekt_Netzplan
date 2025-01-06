726ea2224d11e009ccaa9094df72caa2039fb4d0
Tomx1878 <kitoboardx1878@gmail.com>
2025-01-07 00:31:06 +0100

    neue klasse zum Anlegen von mehreren Netzplaenen.

--- src/main/java/AlleNetzplaene.java
+++ src/main/java/AlleNetzplaene.java
@@ -3,10 +3,43 @@ import java.util.List;
 
 public class AlleNetzplaene {
 
-
+    private static UserInput userInput = new UserInput();
+    private static Menus menus = new Menus();
     private static List<AlleNetzplaene> alleNetzplaene = new ArrayList<>();
+    private String netzplanName = " ";
 
 
+
+    public void netzplanMenu() {
+        switch (menus.netzplanBearbeitenMenu()) {
+            case 1:
+                netzplanName = userInput.readString("Bitte geben Sie den Namen des Netzplanes an.");
+
+                //neuesApAnlegen();
+                break;
+            case 2:
+                for (AlleNetzplaene l : alleNetzplaene) {
+                    System.out.println(l);
+                }
+                break;
+            case 3:
+                String antwort = " ";
+                userInput.jaNeinAbfrage("Möchten Sie zurück zum Haupt menu ?");
+                if (antwort.equals("Y") || antwort.equals("y")) {
+                    menus.startMenuAuswahl();
+                } else {
+                    netzplanMenu();
+                }
+
+                break;
+            /* case 4:
+
+                break;
+            case 5: */
+            default:
+                throw new RuntimeException("WURDE NOCH NICHT PROGRAMMIERT");
+        }
+    }
 
 
     // Getter und Setter für alleNetzplaene.
@@ -15,5 +48,13 @@ public class AlleNetzplaene {
     }
     public static void setAlleNetzplaene(List<AlleNetzplaene> alleNetzplaene) {
         AlleNetzplaene.alleNetzplaene = alleNetzplaene;
+    }
+
+    // GetterSetter für den Netzplan Name.
+    public String getNetzplanName() {
+        return netzplanName;
+    }
+    public void setNetzplanName(String netzplanName) {
+        this.netzplanName = netzplanName;
     }
 }
